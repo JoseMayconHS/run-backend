ad0246f710e584d30dfb8860c6e636ade0fdc651
José Maycon <51084523+Maycon-PE@users.noreply.github.com>
2019-08-30 20:22:30 -0300

    Update connection_config

--- src/services/database/connection_config.js
+++ src/services/database/connection_config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const config = {
-	database: process.env.DATABASE_DATABASE || '',
+	database: process.env.DATABASE_NAME || '',
 	user: process.env.DATABASE_USER || '',
 	password: process.env.DATABASE_PASSWORD || ''
 }
