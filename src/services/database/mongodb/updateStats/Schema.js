4bb3358c4a40f3b1d7749af0ed1816ede6cbd726
Maycon Silva <mayconprodigio@hotmail.com>
2019-09-17 22:36:37 -0300

    Loop of updates

--- src/services/database/mongodb/updateStats/Schema.js
+++ src/services/database/mongodb/updateStats/Schema.js
@@ -2,14 +2,19 @@ const { Schema, model } = require('mongoose')
 
 const Stats = new Schema({
 	users: Number,
-	bots: Number,
-	online: Number
+	bots: Number
+}, {
+	timestamps: true
 })
 
-Stats.pre('save', next => {
-	console.log('novo stats')
+// Stats.pre('save', async next => {
 
-	next()
-})
+// 	const stats = await Stats.findOne()
+
+// 	if 
+// 	Stats.remove({ sort: { 'createdAt': -1 } }, )
+
+// 	next()
+// })
 
 module.exports = model('stats', Stats)
