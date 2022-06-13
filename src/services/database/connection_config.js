64b6627cfce478ad5840600ea4baea2237b7272e
Maycon Silva <jose.maycon.hermogenes.silva@gmail.com>
2022-06-13 15:59:32 -0300

    healthcheck

--- src/services/database/connection_config.js
+++ src/services/database/connection_config.js
@@ -4,7 +4,8 @@ const config = {
 	host: process.env.DATABASE_HOST || '',
 	database: process.env.DATABASE_NAME || '',
 	user: process.env.DATABASE_USER || '',
-	password: process.env.DATABASE_PASSWORD || ''
+	password: process.env.DATABASE_PASSWORD || '',
+	port: process.env.DATABASE_PORT || 3306
 }
 
 module.exports = config
