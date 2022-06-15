f70ca355c08733c4d40da8a1c040363195a841b2
Maycon Silva <jose.maycon.hermogenes.silva@gmail.com>
2022-06-15 10:09:53 -0300

    Apagar imagem na aws | Corrigido

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 
-console.log('Inicializando ....')
-
 const express = require("express");
 const body_parser = require("body-parser");
 const cors = require("cors");
--- src/server/routes/controllers/deletes.js
+++ src/server/routes/controllers/deletes.js
@@ -1,14 +1,23 @@
 const { DeleteObjectCommand } = require('@aws-sdk/client-s3')
 
 const { s3 } = require('../../../services/upload');
-const { remove, selectWhere } = require('../../../services/database/sqlQuery/delete');
+const { selectWhere } = require('../../../services/database/sqlQuery/select');
+const { remove } = require('../../../services/database/sqlQuery/delete');
 
 async function doRemove(req, res) {
   const [user] = await selectWhere("users", { id: req.user }, "src");
 
-  const command = await DeleteObjectCommand(user.src)
+  if (user.src !== 'pilots/default') {
+    const Key = user.src.split('amazonaws.com/')[1]
 
-  await s3.send(command)
+    const command = new DeleteObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key
+    })
+
+    await s3.send(command)
+  }
+
 
   const result = await remove({ user: req.user, car: req.car })
 
